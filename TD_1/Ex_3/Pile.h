c98589e3c63fe922589ddf825464847a4635bb85
bahhar.zakariya <74328672+BAHHAR@users.noreply.github.com>
2020-11-18 12:52:20 +0100

    Update Pile.h

--- TD_1/Ex_3/Pile.h
+++ TD_1/Ex_3/Pile.h
@@ -4,10 +4,12 @@ class Pile
 public:
 	Pile();
 	Pile(int);
-	void m_empiler(int);
-	void m_depiler(int);
+	void m_empiler(const int&);
+	void m_depiler(const int&);
 	bool m_pileVide();
 	void m_afficherPile()const;
+	int m_sommet();
+	void m_inverserPile();
 	~Pile();
 private:
 	int sommet,*table,taille;
