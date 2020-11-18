aa56d9990c871d61c3fcfca5af0091d0f614fd3d
bahhar.zakariya <74328672+BAHHAR@users.noreply.github.com>
2020-11-18 12:50:56 +0100

    Update Pile.cpp

--- TD_1/Ex_3/Pile.cpp
+++ TD_1/Ex_3/Pile.cpp
@@ -15,46 +15,63 @@ Pile::Pile(int taille)
 	sommet = -1;
 }
 
-void Pile::m_empiler(int valeur)
+void Pile::m_empiler(const int& valeur)
 {
 	int i = sommet + 1;
 	if (m_pileVide()) {
-	table[i] =valeur;
-	sommet++;
+		table[i] = valeur;
+		sommet++;
 	}
 	else
-		cout << "La pile est pleine!"<<endl;
+		cout << "La pile est pleine!" << endl;
 }
 
-void Pile::m_depiler(int valeur)
+void Pile::m_depiler(const int& valeur)
 {
 	if (sommet > 0) {
 		do {
-			cout <<table[sommet];
+			cout << table[sommet];
 			sommet--;
 			if (table[sommet] == valeur) {
 				cout << table[sommet];
 				break;
 			}
-		} while (sommet!=-1);
+		} while (sommet != -1);
 	}
 }
 
 bool Pile::m_pileVide()
 {
-	if(sommet<taille)
+	if (sommet < taille)
 		return true;
 
 }
 
 void Pile::m_afficherPile() const
 {
-	for (int i = 0; i <taille; i++) {
+	for (int i = 0; i < taille; i++) {
 
-	cout << table[i];
+		cout << table[i];
 	}
 	cout << endl;
 }
-Pile::~Pile(){
-	delete [] table;
+int Pile::m_sommet()
+{
+	return table[sommet];
+}
+void Pile::m_inverserPile()
+{
+	int indice = taille-1;
+	for (int i = 0; i < taille; i++) {
+		int aide =table[i];
+		table[i] =table[indice];
+		table[indice] = aide;
+		indice--;
+		if (indice == 0)
+			break;
+	}
+	
+}
+Pile::~Pile() {
+	delete[] table;
 }
