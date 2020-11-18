380279f6428adaa087f3114b92d9b31f300fa979
bahhar.zakariya <74328672+BAHHAR@users.noreply.github.com>
2020-11-18 13:02:18 +0100

    Update Ex_3.cpp

--- TD_1/Ex_3/Ex_3.cpp
+++ TD_1/Ex_3/Ex_3.cpp
@@ -7,7 +7,11 @@ int main()
     a.m_empiler(2);
     a.m_empiler(3);
     a.m_afficherPile();
-    a.m_depiler(1);
+    a.m_depiler(2);//depiler la valeur
+    std::cout << std::endl;
+    std::cout <<"La valeur du sommet: " <<p1.m_sommet() << std::endl; 
+    p1.m_inverserPile();
+    p1.m_afficherPile();
 }
 
 
